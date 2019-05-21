825a9436930cf201fe52e619c028d619ecbb11e0
dumboz <heydumboz@gmail.com>
2019-05-21 13:25:11 +0800

    update range.js 增加了输出为数组的版本

--- range.js
+++ range.js
@@ -1,3 +1,4 @@
+// 使用generator实现的版本
 const range = function* (start, end, step = 1) {
   let x = start;
   while (x < end) {
@@ -6,4 +7,18 @@ const range = function* (start, end, step = 1) {
   }
 }
 
-module.exports = range
+// 如果想得到一个数组
+
+const rangeAsArray =  (start, end, step = 1) => {
+  return Array.from({length: Math.ceil((end - start) / step)}, (v, i) => {
+    return start + i * step;
+  } )
+}
+
+// console.log(rangeAsArray(2,9,2))
+
+// 当然也可以这样
+
+const rangeAsArray2 = (...args) => [...range(...args)];
+
+// console.log(rangeAsArray2(2,9,2))
